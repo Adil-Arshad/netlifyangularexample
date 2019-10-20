81acf3df3fc883b533431cd30a9a9be71e26d254
Adil-Arshad <adilarshad3347@gmail.com>
2019-10-20 13:39:03 +0500

    set up angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { XyzComponent } from './xyz/xyz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XyzComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/xyz/xyz.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-xyz',
+  templateUrl: './xyz.component.html',
+  styleUrls: ['./xyz.component.scss']
+})
+export class XyzComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
